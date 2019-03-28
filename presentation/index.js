7f473b2629f77d646b32e7aeb0848ebf2d2ea1b6
Kajetan Swiatek <kajetan.swiatek@gmail.com>
2019-03-28 19:21:08 +0100

    added slides * changed images directory

--- presentation/index.js
+++ presentation/index.js
@@ -14,43 +14,42 @@ import createTheme from 'spectacle/lib/themes/default';
 
 // Assets
 const images = {
-  tenYearsChallenge: require('../assets/js-10yearschallenge-meme.jpg'),
-  jsOldLogo: require('../assets/js-logo-old.png'),
-  jsNewLogo: require('../assets/js-logo-new.png'),
-  jsLogo: require('../assets/js-logo.jpg'),
-  busted: require('../assets/busted.png'),
-  jqueryLogo: require('../assets/jquery-logo.png'),
-  nodejsLogo: require('../assets/nodejs-logo.png'),
-  jreNodejs: require('../assets/jre-nodejs.png'),
-  npmLogo: require('../assets/npm-logo.png'),
-  programming: require('../assets/programming.gif'),
-  expressLogo: require('../assets/express-logo.png'),
-  nestLogo: require('../assets/nest-logo.png'),
-  angularLogo: require('../assets/angular-logo.png'),
-  reactLogo: require('../assets/react-logo.png'),
-  vueLogo: require('../assets/vue-logo.png'),
-  cypressLogo: require('../assets/cypress-logo.jpg'),
-  jestLogo: require('../assets/jest-logo.png'),
-  webpackLogo: require('../assets/webpack-logo.png'),
-  gulpLogo: require('../assets/gulp-logo.png'),
-  gruntLogo: require('../assets/grunt-logo.png'),
-  electronLogo: require('../assets/electron-logo.png'),
-  ionicLogo: require('../assets/ionic-logo.png'),
-  tensorflowLogo: require('../assets/tensorflow-logo.png'),
-  brainLogo: require('../assets/brain-logo.png'),
-  bitcoinLogo: require('../assets/bitcoin-logo.png'),
-  devpun: require('../assets/devpun.gif'),
-  thanos: require('../assets/thanos.png'),
-  tc: require('../assets/tc39.png'),
-  ieTroll: require('../assets/ie-troll.png'),
-  freecodecamp: require('../assets/freecodecamp.png'),
-  mosh: require('../assets/mosh.png'),
-  academind: require('../assets/academind.png')
+  tenYearsChallenge: require('../assets/images/js-10yearschallenge-meme.jpg'),
+  jsOldLogo: require('../assets/images/js-logo-old.png'),
+  jsNewLogo: require('../assets/images/js-logo-new.png'),
+  jsLogo: require('../assets/images/js-logo.jpg'),
+  busted: require('../assets/images/busted.png'),
+  jqueryLogo: require('../assets/images/jquery-logo.png'),
+  nodejsLogo: require('../assets/images/nodejs-logo.png'),
+  jreNodejs: require('../assets/images/jre-nodejs.png'),
+  npmLogo: require('../assets/images/npm-logo.png'),
+  programming: require('../assets/images/programming.gif'),
+  expressLogo: require('../assets/images/express-logo.png'),
+  nestLogo: require('../assets/images/nest-logo.png'),
+  angularLogo: require('../assets/images/angular-logo.png'),
+  reactLogo: require('../assets/images/react-logo.png'),
+  vueLogo: require('../assets/images/vue-logo.png'),
+  cypressLogo: require('../assets/images/cypress-logo.jpg'),
+  jestLogo: require('../assets/images/jest-logo.png'),
+  webpackLogo: require('../assets/images/webpack-logo.png'),
+  gulpLogo: require('../assets/images/gulp-logo.png'),
+  gruntLogo: require('../assets/images/grunt-logo.png'),
+  electronLogo: require('../assets/images/electron-logo.png'),
+  ionicLogo: require('../assets/images/ionic-logo.png'),
+  tensorflowLogo: require('../assets/images/tensorflow-logo.png'),
+  brainLogo: require('../assets/images/brain-logo.png'),
+  bitcoinLogo: require('../assets/images/bitcoin-logo.png'),
+  thanos: require('../assets/images/thanos.png'),
+  tc: require('../assets/images/tc39.png'),
+  ieTroll: require('../assets/images/ie-troll.png'),
+  freecodecamp: require('../assets/images/freecodecamp.png'),
+  mosh: require('../assets/images/mosh.png'),
+  academind: require('../assets/images/academind.png')
 };
 
 const code = {
   oop: require("raw-loader!../assets/code/oop.example"),
-  start: require("raw-loader!../assets/code/start.example"),
+  weakly: require("raw-loader!../assets/code/weakly.example"),
   fp: require("raw-loader!../assets/code/fp.example"),
   backend: require("raw-loader!../assets/code/backend.example"),
   frontend: require("raw-loader!../assets/code/frontend.example"),
@@ -62,8 +61,10 @@ require('normalize.css');
 
 const colors = {
   white: 'white',
+  black: 'black',
   lightblue: '#03A9FC',
-  pink: '#EB4F80'
+  pink: '#EB4F80',
+  red: '#f44242'
 };
 
 const theme = createTheme(
@@ -84,7 +85,7 @@ export default class Presentation extends React.Component {
     return (
       <Deck
         theme={theme}
-        transition={['slide', 'spin']}
+        transition={['fade']}
         transitionDuration={500}
         contentHeight={800}
       >
@@ -110,9 +111,12 @@ export default class Presentation extends React.Component {
           </Appear>
         </Slide>
 
-        <Slide>
+        <Slide
+          bgImage="https://images.pexels.com/photos/355952/pexels-photo-355952.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"
+          bgDarken="0.5"
+        >
           <Heading
-            size={1} 
+            size={1}
             textColor={colors.lightblue}
             lineHeight={1.2}
             margin="0 0 3rem 0"
@@ -129,36 +133,42 @@ export default class Presentation extends React.Component {
           </BlockQuote>
         </Slide>
 
-        <Slide>
-          <Heading textColor={colors.lightblue} size={4} margin="0 0 5rem 0">
+        <Slide
+          bgImage="http://hdqwalls.com/wallpapers/code.jpg"
+          bgDarken="0.5"
+        >
+          <Heading textColor={colors.white} size={1}>
             JavaScript as a programming language
           </Heading>
-          <List textColor={colors.white}>
-            <Appear>
-              <ListItem padding=".5rem">Weakly typed</ListItem>
-            </Appear>
-            <Appear>
-              <ListItem padding=".5rem">Single-threaded</ListItem>
-            </Appear>
-            <Appear>
-              <ListItem padding=".5rem">Synchronous</ListItem>
-            </Appear>
-          </List>
+        </Slide>
+
+        <Slide>
+          <Heading textColor={colors.lightblue} size={2} margin="0 0 5rem 0">
+            Weakly typed
+          </Heading>
+          <CodePane
+            lang="js"
+            source={code.weakly}
+            textSize="1.5rem"
+          />
         </Slide>
 
         <Slide>
           <Heading
-            textColor={colors.lightblue}
-            size={2}
-            margin="0 0 3rem 0"
+            textColor={colors.red}
+            size={1}
+            margin="0 0 5rem 0"
+            style={{ textDecoration: 'line-through' }}
           >
-            For starters...
+            Asynchronous
           </Heading>
-          <CodePane
-            lang="js"
-            source={code.start}
-            textSize="1.5rem"
-          />
+          <Heading
+            textColor={colors.lightblue}
+            size={1}
+            margin="0 0 5rem 0"
+          >
+            Synchronous <br/> & <br/> Single-threaded
+          </Heading>
         </Slide>
 
         <Slide>
@@ -177,7 +187,7 @@ export default class Presentation extends React.Component {
           <Anim
             transitionDuration={500}
             fromStyle={{ opacity: 0, transform: 'translate(0px, 0px)' }}
-            toStyle={[{ opacity: 1, transform: 'translate(400px, -700px) rotate(-10deg)' }]}
+            toStyle={[{ opacity: 1, transform: 'translate(400px, -500px) rotate(-10deg)' }]}
             easing={'back'}
           >
             <div>
@@ -211,7 +221,10 @@ export default class Presentation extends React.Component {
           </Anim>
         </Slide>
 
-        <Slide>
+        <Slide
+          bgImage="https://images.pexels.com/photos/924824/pexels-photo-924824.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+          bgDarken="0.3"
+        >
           <Image src={images.jsOldLogo} height={200}/>
           <Heading
             size={2}
@@ -305,10 +318,13 @@ export default class Presentation extends React.Component {
 
         <Slide bgImage={images.programming}></Slide>
 
-        <Slide>
+        <Slide
+          bgImage="https://images.unsplash.com/reserve/oIpwxeeSPy1cnwYpqJ1w_Dufer%20Collateral%20test.jpg?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1216&q=80"
+          bgDarken="0.6"
+        >
           <Heading
-            size={2}
-            textColor={colors.pink}
+            size={1}
+            textColor={colors.lightblue}
             margin="0 0 3rem 0"
           >
             What is the use for JavaScript today?
@@ -326,14 +342,20 @@ export default class Presentation extends React.Component {
           <Image
             src={images.expressLogo}
             display="inline"
-            height={100}
+            height={80}
             margin="0 5rem 3rem 0"
           />
           <Image
             src={images.nestLogo}
             display="inline"
-            height={100}
+            height={80}
             margin="0 5rem 3rem 0"
+          />
+          <Image
+            src="https://raw.githubusercontent.com/marblejs/marble/master/assets/img/logo.png"
+            display="inline"
+            height={80}
+            margin="0 0 3rem 0"
           />
           <CodePane
             lang="js"
@@ -487,19 +509,14 @@ export default class Presentation extends React.Component {
         <Slide>
           <Heading
             textColor={colors.lightblue}
-            size={2}
+            size={1}
             margin="0 0 3rem 0"
           >
             Having fun!
           </Heading>
-          <Image
-            src={images.devpun}
-            width={1000}
-          />
           <Appear>
             <Image
               src={images.thanos}
-              style={{ position: 'fixed', transform: 'translate(700px, -200px)' }}
               width={300}
             />
           </Appear>
@@ -537,7 +554,7 @@ export default class Presentation extends React.Component {
           </List>
         </Slide>
 
-        <Slide>
+        <Slide bgImage="https://images.pexels.com/photos/356079/pexels-photo-356079.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" bgDarken="0.8">
           <Heading
             textColor={colors.lightblue}
             size={4}
@@ -565,34 +582,7 @@ export default class Presentation extends React.Component {
         </Slide>
 
         <Slide>
-          <Heading
-            textColor={colors.lightblue}
-            size={3}
-            margin="0 0 2rem 0"
-          >
-            Where to learn it?
-          </Heading>
-          <Link href="https://learn.freecodecamp.org/" target="_blank">
-            <Image
-              src={images.freecodecamp}
-              display="inline"
-              height={300}
-            />
-          </Link>
-          <Link href="https://www.youtube.com/watch?v=W6NZfCO5SIk&list=PLTjRvDozrdlxEIuOBZkMAK5uiqp8rHUax" target="_blank">
-            <Image
-              src={images.mosh}
-              display="inline"
-              height={300}
-            />
-          </Link>
-          <Link href="https://www.academind.com/learn/javascript/" target="_blank">
-            <Image
-              src={images.academind}
-              display="inline"
-              height={300}
-            />
-          </Link>
+          <Image src="http://sogrady-media.redmonk.com/sogrady/files/2019/03/lang.rank_.119.png"/>
         </Slide>
 
         <Slide>
@@ -606,16 +596,6 @@ export default class Presentation extends React.Component {
             <ListItem>
               <Link href="https://medium.com/@kvosswinkel/is-javascript-synchronous-or-asynchronous-what-the-hell-is-a-promise-7aa9dd8f3bfb" target="_blank" textColor={colors.white}>
                 Is JS synchronous or asynchronous?
-              </Link>
-            </ListItem>
-            <ListItem>
-              <Link href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Working_with_Objects" target="_blank" textColor={colors.white}>
-                Objects in JS
-              </Link>
-            </ListItem>
-            <ListItem>
-              <Link href="https://developer.mozilla.org/pl/docs/Web/JavaScript/Reference/Functions" target="_blank" textColor={colors.white}>
-                Functions in JS
               </Link>
             </ListItem>
             <ListItem>
@@ -657,11 +637,53 @@ export default class Presentation extends React.Component {
         </Slide>
 
         <Slide>
-          <CodePane
-            lang="js"
-            source={'console.log(\'Thank you!\');'}
-            textSize="5rem"
-          />
+          <Heading
+            textColor={colors.lightblue}
+            size={3}
+            margin="0 0 2rem 0"
+          >
+            Begin your journey!
+          </Heading>
+          <Link href="https://learn.freecodecamp.org/" target="_blank">
+            <Image
+              src={images.freecodecamp}
+              display="inline"
+              height={250}
+            />
+          </Link>
+          <Link href="https://www.youtube.com/watch?v=W6NZfCO5SIk&list=PLTjRvDozrdlxEIuOBZkMAK5uiqp8rHUax" target="_blank">
+            <Image
+              src={images.mosh}
+              display="inline"
+              height={250}
+            />
+          </Link>
+          <Link href="https://www.academind.com/learn/javascript/" target="_blank">
+            <Image
+              src={images.academind}
+              display="inline"
+              height={280}
+            />
+          </Link>
+          <Text margin="2rem 0" textColor={colors.white}>(Also via communities! 😊)</Text>
+        </Slide>
+
+        <Slide>
+          <Heading>Thank you! 😊</Heading>
+          <section style={{ marginTop: '4rem' }}>
+            <Image
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/31/Blogger.svg/1030px-Blogger.svg.png"
+              height={100}
+            />
+            <Text textColor={colors.white}>dev-self-start.blogspot.com</Text>
+          </section>
+          <section style={{ marginTop: '4rem' }}>
+            <Image
+              src="https://seeklogo.com/images/T/twitter-2012-negative-logo-5C6C1F1521-seeklogo.com.png"
+              height={100}
+            />
+            <Text textColor={colors.white}>@KajetanSw</Text>
+          </section>
         </Slide>
       </Deck>
     );
